be1ac7508e466b59c60f7edf6cf9963aa243de13
UkhuReaper <mikael.f.krog@gmail.com>
2019-11-03 04:48:12 +0100

    Added hashMaps

--- src/main/java/bookProject/BookRegister.java
+++ src/main/java/bookProject/BookRegister.java
@@ -1,29 +1,39 @@
 package bookProject;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Scanner;
+import java.util.*;
 
 
 class BookRegister {
 
     private final ArrayList<Book> bookList;
+    private HashMap<String, Book> bookHashMap;
 
     //Constructor
     BookRegister(){
         bookList = new ArrayList<>();
+        bookHashMap = new HashMap<>();
     }
 
     //Adds 4 test books to the library
     void addTestBooks() {
-        bookList.add(new Book("Title1", "Author1", "Publisher1", 2019, 500, "1234567890123", false));
-        bookList.add(new Book("Title2", "Author1", "Publisher2", 2018, 600, "1234567890124", true));
-        bookList.add(new Book("Title3", "Author1", "Publisher3", 2017, 700, "1234567890125", true));
-        bookList.add(new Book("Title4", "Author1", "Publisher4", 2016, 800, "1234567890126", false));
+        Book book1 = new Book("Title1", "Author1", "Publisher1", 2019, 500, "1234567890123", false);
+        Book book2 = new Book("Title2", "Author2", "Publisher2", 2019, 500, "1234567890124", false);
+        Book book3 = new Book("Title3", "Author3", "Publisher3", 2019, 500, "1234567890125", false);
+        Book book4 = new Book("Title4", "Author4", "Publisher4", 2019, 500, "1234567890126", false);
+        bookList.add(book1);
+        bookList.add(book2);
+        bookList.add(book3);
+        bookList.add(book4);
+        bookHashMap.put("1234567890123", book1);
+        bookHashMap.put("1234567890124", book2);
+        bookHashMap.put("1234567890125", book3);
+        bookHashMap.put("1234567890126", book4);
     }
 
     //method which adds a book to the library
     void addBook(String title, String author, String publisher, int yearReleased, int pages, String EAN, boolean loaned) {
-        bookList.add(new Book(title, author, publisher, yearReleased, pages, EAN, loaned));
+        Book book = new Book(title, author, publisher, yearReleased, pages, EAN, loaned);
+        bookList.add(book);
+        bookHashMap.put(EAN, book);
     }
 
     Book searchBookTitle(String searchBook) {
@@ -50,11 +60,11 @@ class BookRegister {
     }
 
     Book searchBookEAN(String ean) {
-        Iterator<Book> bookIterator = this.getIterator();
+        Iterator bookIterator = bookHashMap.entrySet().iterator();
         while(bookIterator.hasNext()) {
-            Book book = bookIterator.next();
-            if(book.getBookEAN().toLowerCase().equals(ean.toLowerCase())) {
-                return book;
+            Map.Entry mapElement = (Map.Entry)bookIterator.next();
+            if(mapElement.getKey().equals(ean)) {
+                return (Book)mapElement.getValue();
             }
         }
         return null;
@@ -69,13 +79,14 @@ class BookRegister {
             if (bookList.get(i).getBookTitle().toLowerCase().equals(deleteBook.toLowerCase())
                     || bookList.get(i).getBookAuthor().toLowerCase().equals(deleteBook.toLowerCase())
                     || bookList.get(i).getBookEAN().equals(deleteBook)) {
+                found = true;
                 bookList.get(i).printBook();
                 System.out.println("Type 'yes' if this is the book you wish to remove, type 'no' if you do not wish to remove this book");
                 String ans = sc.next().toLowerCase();
-
                 if(ans.equals("yes")) {
-                    found = true;
+
                     System.out.println("You have deleted " + bookList.get(i).getBookTitle() + " From the bookregistry");
+                    bookHashMap.remove(bookList.get(i).getBookEAN());
                     bookList.remove(i);
                     i--;
                 }
